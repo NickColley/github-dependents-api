131a925f75ea72217ea348a66fb5fb6e1e9611dd
System <none>
2017-05-01 10:06:08 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var proxy = require('http-proxy-middleware');
 var options = {
   logLevel: 'debug',
-  target: 'https://api.airtable.com/v0/' + process.env.APP_ID,
+  target: 'https://api.airtable.com/v0/',
   changeOrigin: true,
   headers: {
     'Accept': 'application/json',
@@ -17,7 +17,13 @@ var options = {
   }
 };
 var apiProxy = proxy(options);
+
 var app = express();
+
+// Setup the status page
+app.use(require('express-status-monitor')());
+
+// Setup the api
 app.use('/api', apiProxy);
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log('Listening on port ' + server.address().port);
