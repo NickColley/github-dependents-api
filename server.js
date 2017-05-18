05e7af3c57ef0a5a3666e3d2d71375631bc38d89
System <none>
2017-05-18 19:14:35 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 const Feedme = require('feedme');
 const parseString = require('xml2js').parseString;
+const parser = require('rss-parser');
+const Feed = require('rss-to-json');
 
 const options = {
   logLevel: 'debug',
@@ -21,16 +23,16 @@ const options = {
     let body = '';
     const feed = new Feedme(true);
     proxyRes.on('data', (data) => {
-      console.log(typeof data, data);
-      feed.write(data.toString('utf-8'));
+      console.log(typeof data, data.toString('utf8'));
+      feed.write(data.toString('utf8'));
     });
     proxyRes.on('end', () => {
-      console.log(body);
-      console.log(typeof body);
-      parseString(body, (err, result) => {
-        console.log(err, result);
-        res.write(result);
-      });
+      console.log('end');
+    });
+    feed.on('end', () => {
+      const data = feed.done();
+      console.log(data);
+      res.write(data);
     });
   }
 };
@@ -43,10 +45,16 @@ app.use(require('express-status-monitor')());
 
 // Setup the api
 app.use('/api', apiProxy);
+app.use('/rss/:data', (req, res, next) => {
+  Feed.load(`https://medium.com/${}`)
+});
+
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log('Listening on port ' + server.address().port);
 });
+
 app.use('/', function (req, res, next) {
   res.send('Ready');
 });
+
 module.exports = app;
