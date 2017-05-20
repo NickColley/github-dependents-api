2658aa655e7b28ca1e9452704c89ce2b865b96dd
System <none>
2017-05-20 08:41:30 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,22 +3,31 @@ const Feed = require('rss-to-json');
 const rsj = require('rsj');
 const cors = require('cors');
 const cache = require('./db/cache');
+const CronJob = require('cron').CronJob;
+
+cons
 
 const app = express();
 
 app.use(cors());
 
 app.get('/rss/:data(*)', (req, res, next) => {
-  cache.find({key: req.params.data}, (err, docs) => {
-    
-  });
-  rsj.r2j(`https://medium.com/${req.params.data}`, (data) => {
+  const processData = (data, alreadyStored) => {
     let parsed = JSON.parse(data);
     const limit = +req.query.limit;
     if (!isNaN(limit)) {
       parsed = parsed.slice(0, limit);
     }
     res.json(parsed);
+  };
+  
+  cache.find({key: req.params.data}, (err, docs) => {
+    if (err) {
+      console.log(err);
+      rsj.r2j(`https://medium.com/${req.params.data}`, processData);
+    } else {
+      processData(docs, true);  
+    }
   });
 });
 
